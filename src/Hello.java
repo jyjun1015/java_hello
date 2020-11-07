a7bedb1595f8ff3e03a14ca028926e9c8748102d
jeong <jeong@DESKTOP-E2EEE3N>
2020-11-08 03:32:00 +0900

    반갑다

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, ¤·¤·");
+		System.out.println("Hello, 1st");
+		System.out.println("Hello, 2nd");
+		
 	}
 
 }
